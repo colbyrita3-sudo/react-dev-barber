e202e9596a70cf8b50f94174be6d3c906636ca6c
colbyrita3-sudo <colbyrita3@gmail.com>
2026-02-19 03:38:24 -0500

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,22 +5,19 @@ export const Footer = () => {
         <h2>Golden Touch Barber Studio</h2>
 
         <p style={{ textAlign: "center", marginTop: "12px" }}>
+          995 W Orange Blossom Trail, Apopka, FL 32712
+        </p>
+
+        <p style={{ textAlign: "center", marginTop: "12px" }}>
+          <a href="tel:+14075575702">(407) 557-5702</a>
+        </p>
+
+        <p style={{ textAlign: "center", marginTop: "12px" }}>
           Walk-ins welcome when available • Appointments recommended
         </p>
 
         <p style={{ textAlign: "center", marginTop: "12px" }}>
           Mon–Fri 10:00 AM – 6:30 PM • Sat 10:00 AM – 5:30 PM • Sun Closed
-        </p>
-
-        <p style={{ textAlign: "center", marginTop: "12px" }}>
-          Instagram:{" "}
-          <a
-            href="https://www.instagram.com/gt_barber_lounge/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            @gt_barber_lounge
-          </a>
         </p>
       </div>
     </footer>
