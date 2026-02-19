2667da20b2349d9802d20152b6243927e03de7d8
colbyrita3-sudo <colbyrita3@gmail.com>
2026-02-19 02:55:55 -0500

    Update index.tsx

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -21,10 +21,10 @@ export const Navbar = () => {
                 </div>
             </nav>
             <main className="hero container" data-aos="fade-up" data-aos-delay="400">
-                <h1>ESTILO É UM REFLEXO DA SUA ATITUDE E SUA PERSONALIDADE.</h1>
-                <p>Horário de funcionamento: 09:00 às 19:00</p>
-                <a href="https://api.whatsapp.com/send?phone=5518999999999&text=Quero%20fazer%20um%20agendamento%20hoje!" className="button-contact" target="_blank">
-                    Agendar horário
+                <h1>Golden Touch Barber Studio.</h1>
+                <p>Private, luxury grooming — precision cuts, clean fades, and beard artistry.</p>
+                <a href="https://booksy.com/en-us/792234_golden-touch-barber-lounge_barber-shop_15924_apopka?hl=en-US&rwg_token=AFd1xnF6HRcnhJ3nNaomEc8XYhKS5qtH--UpzKriTYJqjB6PKz25_yaWqvU_4ow_D1tHHJ7xHWjtLNqs38NU1KUM7vW4ZiuuvQ%3D%3D#ba_s=seo" className="button-contact" target="_blank">
+                    BOOK NOW
                 </a>
             </main>
         </div>
