5a8967e4ff81ebdb79820bb63594586596ab0e2b
colbyrita3-sudo <colbyrita3@gmail.com>
2026-02-19 04:31:47 -0500

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,23 +2,28 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <div className="container footer-content">
-        <h2>Hudsons Barbershop</h2>
+        <h2 style={{ color: "white" }}>Hudsons Barbershop</h2>
 
-        <p style={{ textAlign: "center", marginTop: "12px" }}>
+        <p style={{ textAlign: "center", marginTop: "12px", color: "white" }}>
           515 N Park Ave STE 101, Apopka, FL 32712
         </p>
 
         <p style={{ textAlign: "center", marginTop: "12px" }}>
-          <a href="tel:+14074101316">(407) 410-1316</a>
+          <a
+            href="tel:+14074101316"
+            style={{ color: "white", textDecoration: "underline" }}
+          >
+            (407) 410-1316
+          </a>
         </p>
 
-        <p style={{ textAlign: "center", marginTop: "12px" }}>
+        <p style={{ textAlign: "center", marginTop: "12px", color: "white" }}>
           Walk-ins accepted • Appointments recommended
         </p>
 
-        <p style={{ textAlign: "center", marginTop: "12px" }}>
-          Thu 11–7 • Fri 10–7 • Sat 10–4 • Sun Closed • Mon Closed • Tue 11–6 • Wed
-          11–6
+        <p style={{ textAlign: "center", marginTop: "12px", color: "white" }}>
+          Thu 11–7 • Fri 10–7 • Sat 10–4 • Sun Closed • Mon Closed • Tue 11–6 •
+          Wed 11–6
         </p>
 
         <p style={{ textAlign: "center", marginTop: "12px" }}>
@@ -26,6 +31,7 @@ export const Footer = () => {
             href="https://www.instagram.com/hudsons_barbershop25/"
             target="_blank"
             rel="noreferrer"
+            style={{ color: "white", textDecoration: "underline" }}
           >
             Instagram: @hudsons_barbershop25
           </a>
