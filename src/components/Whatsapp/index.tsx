7dbf9859d057d638070aa8e5731f76680a3d51f7
colbyrita3-sudo <colbyrita3@gmail.com>
2026-02-19 04:41:07 -0500

    Update index.tsx

--- src/components/Whatsapp/index.tsx
+++ src/components/Whatsapp/index.tsx
@@ -1,8 +1,12 @@
 export const BtnWhatsapp = () => {
-    return (
-        <a data-aos="zoom-in-up" data-aos-delay="400" href="https://api.whatsapp.com/send?phone=5518999999999&text=Quero%20fazer%20um%20agendamento%20hoje!" className="btn-whatsapp" target="_blank">
-            <img src="/whatssapp.svg" alt="WhatsApp" />
-            <span className="tooltip-text">Agende seu horário</span>
-        </a>
-    )
-}
+  return (
+    <a
+      className="btn-whatsapp"
+      href="tel:+14074101316"
+      aria-label="Call Hudsons Barbershop"
+      title="Call Hudsons Barbershop"
+    >
+      <i className="fa-solid fa-phone"></i>
+    </a>
+  );
+};
